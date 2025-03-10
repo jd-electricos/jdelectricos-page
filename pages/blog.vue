942d1a1f4bbdcd41e0ba8321ba3a83a9009cca8e
jd-electricos <dispositivosjdelectricos2022@gmail.com>
2025-03-10 13:40:58 -0500

    add new page

---
+++ pages/blog.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="flex flex-col items-center justify-center h-screen">
+      <p class="">{{ message }}</p>
+      <h1 class="text-3xl font-bold underline">{{ message }}</h1>
+    </div>
+  </template>
+  <script setup>
+  const message = "noticias!";
+  </script>
+  
