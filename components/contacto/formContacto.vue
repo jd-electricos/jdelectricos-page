5b785a92db5517ef4dd22ec744720e9058d64471
dispositivosjdelectrico2022 <dispositivosjdelectricos2022@gmail.com>
2025-12-22 16:38:11 -0500

    add captcha api v3

--- components/contacto/formContacto.vue
+++ components/contacto/formContacto.vue
@@ -178,7 +178,7 @@ const dataAffair = ref("");
 const dataMessage = ref("");
 
 const errors = ref({});
-const siteKey = "6LcIktcqAAAAADELG70wM3fL_l9fsdi-RY9tOuzL";
+const siteKey = "6LeRIXgqAAAAAJvfTC0RidvG62l7jYOfQlsO8eQU";
 
 onMounted(() => {
   isRecaptchaVisible.value = true;
@@ -216,7 +216,7 @@ const verifyRecaptcha = async () => {
 
   try {
     const response = await axios.post(
-      "https://clownfish-app-xjood.ondigitalocean.app/api/recaptcha/verify-recaptcha",
+      "https://monkfish-app-lsohl.ondigitalocean.app/api/recaptcha/verify-recaptcha",
       { recaptchaResponse }
     );
 
