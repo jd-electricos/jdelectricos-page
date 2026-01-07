5b44d347ab2ef17af79cab7ba207651e43d91fc7
dispositivosjdelectrico2022 <dispositivosjdelectricos2022@gmail.com>
2026-01-07 15:32:31 -0500

    add styles to index

--- layouts/default.vue
+++ layouts/default.vue
@@ -7,7 +7,8 @@
     </div>
 
     <!-- Contenido principal (Empuja el footer hacia abajo) -->
-    <div class="flex-grow mt-[19.5rem] md:mt-24">
+    <!-- <div class="flex-grow mt-[19.5rem] md:mt-24"> -->
+    <div class="md:mt-24">
       <NuxtPage />
     </div>
 
